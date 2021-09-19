17c4979240147b6157941d13b3076b0447408d1a
phuong7901 <nguyenphuogg79@gmail.com>
2021-09-19 23:11:45 +0700

    homework

---
+++ index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Restaurant website</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <!--Navbar Section Start-->
+  <section class=" navbar">
+    <div class="container">
+     <div class="logo">
+       <img src="./images/logo.png" alt="Restaurant Logo" class=" img-responsive">
+     </div>
+      <div class="menu text-right">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="Categories.html">About</a></li>
+          <li><a href="Food.html">Foods</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div class="clearfix">
+
+      </div>
+    </div>
+    
+  </section>
+  <!--Navbar Section Ends -->
+
+  <!--Food Search Start -->
+  <section class=" food-search text-center">
+  <div class="container">
+    
+    <form action="">
+      <input type="search" name="search" placeholder="Search for Food..">
+      <input type="submit" name="submit" value="Search" class="btn btn-primary">
+    </form>
+    </div>
+    
+  </section>
+  <!--Food Search Ends -->
+
+  <!--Categories Start -->
+  <section class=" categories">
+  <div class="container">
+  <h2 class=" text-center ">Catergories</h2>
+    
+    <div class="Box-3 float-container">
+      <img src="images/pizza.jpg" alt="pizza" class="img-responsive img-curve">
+
+      <h3 class="float-text text-white">Pizza</h3>
+    </div>
+
+    <div class="Box-3 float-container">
+      <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
+
+      <h3 class="float-text text-white">Burger</h3>
+    </div>
+
+    <div class="Box-3 float-container">
+       <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
+
+       <h3 class="float-text text-white">Momo</h3>
+    </div>
+    <div class="clearfix"></div>
+    
+  </section>
+  <!--Categories Ends -->
+
+  <!--Food Menu Start -->
+  <section class="food-menu">
+  <div class="container">
+    <h2 class=" text-center">Explore Foods</h2>
+
+    <div class="food-menu-box">
+     <div class="food-menu-img">
+       <img src="images/menu-pizza.jpg" alt="Chicken Hawain Pizza" class="img-responsive img-curve">
+     </div>
+      <div class="food-menu-desc">
+        <h4>Food Title</h4>
+        <p class= food-price>$2.3</p>
+        <p class=food-detail>
+          Made with Italian Sauce, Chicken, and organice vegetables.
+        </p>
+        <br>
+
+        <a href="Order.html" class="btn btn-primary">Order Now</a>
+      </div>
+      
+    </div>
+    
+
+    <div class="food-menu-box">
+     <div class="food-menu-img">
+       <img src="images/menu-burger.jpg" alt="Chicken Hawain Burger" class="img-responsive img-curve">
+     </div>
+      <div class="food-menu-desc">
+        <h4>Smoky Burger</h4>
+        <p class= food-price>$2.3</p>
+        <p class=food-detail>
+          Made with Italian Sauce, Chicken, and organice vegetables.
+        </p>
+        <br>
+
+        <a href="Order.html" class="btn btn-primary">Order Now</a>
+      </div>
+      
+    </div>
+
+    <div class="food-menu-box">
+     <div class="food-menu-img">
+       <img src="images/menu-burger.jpg" alt="Chicken Hawain Burger" class="img-responsive img-curve">
+     </div>
+      <div class="food-menu-desc">
+        <h4>Nice Burger</h4>
+        <p class= food-price>$2.3</p>
+        <p class=food-detail>
+          Made with Italian Sauce, Chicken, and organice vegetables.
+        </p>
+        <br>
+
+        <a href="Order.html" class="btn btn-primary">Order Now</a>
+      </div>
+      
+    </div>
+
+    <div class="food-menu-box">
+     <div class="food-menu-img">
+       <img src="images/menu-pizza.jpg" alt="Chicken Hawain Pizza" class="img-responsive img-curve">
+     </div>
+      <div class="food-menu-desc">
+        <h4>Food Title</h4>
+        <p class= food-price>$2.3</p>
+        <p class=food-detail>
+          Made with Italian Sauce, Chicken, and organice vegetables.
+        </p>
+        <br>
+
+        <a href="Order.html" class="btn btn-primary">Order Now</a>
+      </div>
+      
+    </div>
+
+    <div class="food-menu-box">
+     <div class="food-menu-img">
+       <img src="images/menu-pizza.jpg" alt="Chicken Hawain Pizza" class="img-responsive img-curve">
+     </div>
+      <div class="food-menu-desc">
+        <h4>Food Title</h4>
+        <p class= food-price>$2.3</p>
+        <p class=food-detail>
+          Made with Italian Sauce, Chicken, and organice vegetables.
+        </p>
+        <br>
+
+        <a href="Order.html" class="btn btn-primary">Order Now</a>
+      </div>
+      
+    </div>
+
+    <div class="food-menu-box">
+     <div class="food-menu-img">
+       <img src="images/menu-momo.jpg" alt="Chicken Hawain Momo" class="img-responsive img-curve">
+     </div>
+      <div class="food-menu-desc">
+        <h4>Chicken Stream Momo</h4>
+        <p class="food-price">$2.3</p>
+        <p class="food-detail">
+          Made with Italian Sauce, Chicken, and organice vegetables.
+        </p>
+        <br>
+
+        <a href="Order.html" class="btn btn-primary">Order Now</a>
+      </div>
+     </div>
+
+     <div class="clearfix"></div>
+
+    </div>
+  </section>
+  <!--Food Menu Ends here-->
+
+  <!--Social Start here-->
+  <section class=" social">
+  <div class="container text-center">
+    <ul>
+      <li>
+        <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" alt="Facebook"></a>
+      </li>
+      <li>
+        <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="Instagram"></a>
+      </li>
+      <li>
+        <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt=""></a>
+      </li>
+    </ul>
+    </div>
+  </section>
+  <!--Social Ends here-->
+
+  <!--Footer Start here-->
+  <section class=" footer">
+  <div class="container text-center">
+    <p>All right reserved. Desgin by <a href="#">Phuongg</a> </p>
+    </div>
+  </section>
+  <!--Footer Ends here-->
+  
+
+</html>
